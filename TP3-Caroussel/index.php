847cc20debe32df420b8df0aefbc34b2df4b4f48
khadim Niass <niassssn@gmail.com>
2022-03-20 10:40:39 +0000

    carroussel : fait

--- TP3-Caroussel/index.php
+++ TP3-Caroussel/index.php
@@ -1,57 +1,33 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>caroussel</title>
+    <title>TP-3 carroussel</title>
 </head>
-
 <body>
-    <div class="section">
-        <div class="slider">
-            <div class="slide">
-                <input type="radio" name="radio1" id="radio1">
-                <input type="radio" name="radio1" id="radio2">
-                <input type="radio" name="radio1" id="radio3">
-                <input type="radio" name="radio1" id="radio4">
-                <input type="radio" name="radio1" id="radio4">
-                <!-- st = items -->
-                <div class="items">
-                    <img src="imgs/emb1.jpg" alt="">
-                </div>
-                <div class="items">
-                    <img src="imgs/emb2.jpg" alt="">
-                </div>
-                <div class="items">
-                    <img src="imgs/p2.jpg" alt="">
-                </div>
-                <div class="items">
-                    <img src="imgs/pizza.jpg" alt="">
-                </div>
-                <div class="items">
-                    <img src="imgs/soupe.jpg" alt="">
-                </div>
-                <!-- end items -->
-                <div class="nav-auto">
-                    <div class="a-b1"></div>
-                    <div class="a-b2"></div>
-                    <div class="a-b3"></div>
-                    <div class="a-b4"></div>
-                    <div class="a-b5"></div>
-                </div>
-            </div>
-            <div class="nav-m">
-                <label for="radio1"></label>
-                <label for="radio2"></label>
-                <label for="radio3"></label>
-                <label for="radio4"></label>
-                <label for="radio5"></label>
-            </div>
+    <div class="caroussel">
+        <button class="caroussel_button carousel-left"> < </button>
+        <div class="slide-containner"> <!-- caroussel_track-containner -->
+            <ul class="slides">  <!-- caroussel_track -->
+                <li class="caroussel-slide current-slide"><img src="imgs/emb1.jpg" alt="img 1"></li>
+                <li class="caroussel-slide"><img src="imgs/emb2.jpg" alt="img 2"></li>
+                <li class="caroussel-slide"><img src="imgs/p2.jpg" alt="img 3"></li>
+                <li class="caroussel-slide"><img src="imgs/pizza.jpg" alt="img 4"></li>
+                <li class="caroussel-slide"><img src="imgs/soupe.jpg" alt="img 5"></li>
+            </ul>
+        </div>
+        <button class="caroussel_button caroussel-right"> > </button>
+        <div class="caroussel_nav">
+            <button class="caroussel-navigator carousel-current"></button>
+            <button class="caroussel-navigator"></button>
+            <button class="caroussel-navigator"></button>
+            <button class="caroussel-navigator"></button>
+            <button class="caroussel-navigator"></button>
         </div>
     </div>
-<script src="script.js"></script>
+    <script src="script.js"></script>
 </body>
-</html>
+</html> 
